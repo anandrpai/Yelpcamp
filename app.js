2115b9c3b1f0382bcfa59f008baab7750170d88b
Anand R Pai <rpai.anand@gmail.com>
2019-06-12 14:45:24 +0000

    added environment variable

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb+srv://anandrpai:Anand105@yelpcamp-edzgl.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+//mongoose.connect("mongodb+srv://anandrpai:Anand105@yelpcamp-edzgl.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
